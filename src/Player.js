b818c5b7a37a03afd11bf5c6f59a90558332b583
James Cho and Maxwell Dubin <maxwell@>
2019-01-09 16:34:45 -0800

    Shows stats on battle scene, attacks until enemy is defeated

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,5 @@
-export class Player{
-     constructor(classType, speed, health, defense, attack) {
+export class Player {
+    constructor(classType, speed, health, defense, attack) {
         this.classType = classType;
         this.speed = speed;
         this.health = health;
@@ -7,43 +7,73 @@ export class Player{
         this.attack = attack;
     }
 
-    setAttributes(classType){
-        if(classType=="warrior"){
+    setAttributes(classType) {
+        if (classType == "warrior") {
             this.speed = 100;
             this.health = 200;
             this.defense = 100;
             this.attack = 200;
-        } else if( classType=="mage"){
+        } else if (classType == "mage") {
             this.speed = 150;
             this.health = 100;
             this.defense = 50;
             this.attack = 250;
-        } else if(classType=="paladin"){
+        } else if (classType == "paladin") {
             this.speed = 50;
             this.health = 150;
             this.defense = 200;
             this.attack = 150;
-        } else if(classType=="undead"){
+        } else if (classType == "undead") {
             this.speed = 50;
             this.health = 50;
             this.defense = 50;
             this.attack = 300;
-        } else if(classType=="archer"){
+        } else if (classType == "archer") {
             this.speed = 150;
             this.health = 50;
             this.defense = 50;
             this.attack = 300;
-    } else if(classType=="ogre"){
-        this.speed = 5;
-        this.health = 5;
-        this.defense = 5;
-        this.attack = 30;
-    } else if(classType=="demon"){
-        this.speed = 15;
-        this.health = 5;
-        this.defense = 5;
-        this.attack = 30;
-    } 
+        } else if (classType == "ogre") {
+            this.speed = 5;
+            this.health = 500;
+            this.defense = 50;
+            this.attack = 30;
+        } else if (classType == "demon") {
+            this.speed = 15;
+            this.health = 500;
+            this.defense = 50;
+            this.attack = 30;
+        }
+    }
+
+    calculateDamage(player, enemy){   
+       let damage = player.attack - enemy.defense;
+       console.log(damage);   
+       return damage;
+    }
+
+
+    damage(damageAmt, target){
+        console.log(target.health);
+       target.health -= damageAmt;
+       console.log(target.health);
+       this.health = target.health;
+       return this.health
+    }
+
+    playerAlive(player) {
+        if (player.health < 0) {
+            alert(player.classType+ " has died. Game restarting.")
+            document.reload();
+        }
+    }
+
+    enemyAlive(enemy) {
+        if (enemy.health < 0) {
+           return false;
+        } else {
+            return true;
+        }
     }
 
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { GameManager } from './GameManager.js';
 import { Player } from './Player.js';
+
+let newPlayer, newEnemy;
 
 $(document).ready(function () {
   let enemies = document.getElementsByClassName("enemies");
@@ -11,36 +12,105 @@ $(document).ready(function () {
   let horde = document.getElementById("horde");
   console.log(horde);
   $("#horde").addClass("invisible");
+
+  // Choose player character
   $(".characters").click(function (event) {
     // let game = new GameManager;
     let clickedId = this.getAttribute("id");
-    let newPlayer = new Player(clickedId);
+    newPlayer = new Player(clickedId);
     newPlayer.setAttributes(clickedId);
-    console.log(newPlayer);
 
     $(".characters").addClass("invisible");
    
     let playerImg = document.getElementById(clickedId);
     playerImg.classList.remove("invisible");
+    playerImg.classList.add("pickedCharacter");
 
-    let footer = document.getElementById("footer");
+    let stats = document.getElementById("stats");
     let header = document.getElementById("header");
-  
+    let playerStats = document.getElementById("playerStats");
 
     header.innerHTML = "Your player is " + clickedId + "!";
-    footer.innerHTML = "<h3>Your player's stats: </h3>" + " <p>Class: " + newPlayer.classType + "</p>  <p>Speed: " + newPlayer.speed + "</p><p> Health: " + newPlayer.health + " </p><p>Defense: " + newPlayer.defense + "</p><p> Attack: " + newPlayer.attack; +  "</p>";
+    playerStats.innerHTML = "<h3>Your player's stats: </h3>" + " <p>Class: " + newPlayer.classType + "</p>  <p>Speed: " + newPlayer.speed + "</p><p> Health: " + newPlayer.health + " </p><p>Defense: " + newPlayer.defense + "</p><p> Attack: " + newPlayer.attack; +  "</p>";
  
   $("#horde").removeClass("invisible");
   enemies.innerHTML = "<h3>Choose enemies below.</h3>";
 });
+
+// Choose an enemy to fight
 $(".enemies").click(function (event) {
   let clickedId = this.getAttribute("id");
-  let newEnemy = new Player(clickedId);
+  newEnemy = new Player(clickedId);
   newEnemy.setAttributes(clickedId);
-  console.log(newEnemy);
+
+  $(".enemies").addClass("invisible");
+   
+    let enemyImg = document.getElementById(clickedId);
+    enemyImg.classList.remove("invisible");
+    enemyImg.classList.add("pickedEnemy");
+
+  let battle = document.getElementById("battle");
+  battle.innerHTML = "<button id='battle'>Start Battle!</button>"
+  
+});
+
+$("#battle").click(function(event){
+  // let newChar = 
+  let myId = $(".pickedCharacter")[0].getAttribute("id");
+  let myCharacter = new Player(myId);
+  myCharacter.setAttributes(myId);
+
+  // let attack = document.getElementById("attack");
+
+  let enemyId = $(".pickedEnemy")[0].getAttribute("id");
+  let myEnemy = new Player(enemyId);
+  myEnemy.setAttributes(enemyId);
+
+  let battleScene = [myCharacter, myEnemy]
+ 
+  console.table(battleScene);
+  let battleField = document.getElementById("battleField");
+  battleField.innerHTML = "<p>Enemy: " + battleScene[1].classType + "</p>" +
+                            "<p>Enemy Health: " + battleScene[1].health + "</p>"+
+                            "<p>Enemy Attack: " + battleScene[1].attack + "</p>"+
+                            "<p>Enemy Defense: " + battleScene[1].defense + "</p>"+
+                            "<p>Enemy Speed: " + battleScene[1].speed + "</p>" +
+                            "<button id='attack'>Attack!</button>"
+
+                            $("#attack").click(function(event){
+
+                               let dmg = myCharacter.calculateDamage(myCharacter, myEnemy);
+                              myEnemy.damage(dmg, myEnemy);
+                              
+                              let enemyDmg = myEnemy.calculateDamage(myEnemy, myCharacter);
+                              myCharacter.damage(enemyDmg, myCharacter)
+                              
+                              newPlayer = myCharacter;
+                              newEnemy = myEnemy;
+                              
+                              myCharacter.playerAlive(myCharacter);
+                              let enemyIsAlive = true;
+                              let counter=0;
+                              while(enemyIsAlive){
+                                myEnemy.damage(dmg, myEnemy);
+                                counter++;
+                                console.log("Player has attacked." + counter + " times");
+
+                              battleField.innerHTML = "<p>Enemy: " + battleScene[1].classType + "</p>" +
+                              "<p>Enemy Health: " + battleScene[1].health + "</p>"+
+                              "<p>Enemy Attack: " + battleScene[1].attack + "</p>"+
+                              "<p>Enemy Defense: " + battleScene[1].defense + "</p>"+
+                              "<p>Enemy Speed: " + battleScene[1].speed + "</p>" 
+                              
+                              "<button id='attack'>Attack!</button>"
+                              enemyIsAlive = myEnemy.enemyAlive(myEnemy)
+                              }
+                              // attack.innerHTML = "<p>Enemies health is: </p>" + myEnemy.health + "<br> Damage to me: " + myCharacter.damage;
+                            
+                            });
 });
 
 
-
 });
+  
 
